29247f1cd3c8a7851f996ad38a49b7846bcff80e
Eelke Blok <eelke@blokspeed.net>
2017-12-22 15:58:42 +0100

    Also remove update manager configuration when exporting config

--- scripts/export-config.sh
+++ scripts/export-config.sh
@@ -9,8 +9,9 @@ rm config/install/*
 cp data/config/sync/* config/install/
 # See https://www.drupal.org/docs/8/creating-distributions/how-to-write-a-drupal-8-installation-profile#config
 # To get things to work on a Mac we need to specify the extension for the -i
-# option. We subsequently delete the backup files.
+# option for sed. We subsequently delete the backup files.
 rm config/install/core.extension.yml
+rm config/install/update.settings.yml
 find config/install/ -type f -exec sed -i.bak -e '/^uuid: /d' {} \;
 rm config/install/*.bak
 find config/install/ -type f -exec sed -i.bak -e '/^_core:/{N;d;}' {} \;
